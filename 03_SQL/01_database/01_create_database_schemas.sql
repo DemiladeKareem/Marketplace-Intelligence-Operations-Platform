1b4c5386b806fab92533e957f84d662494a46d31
Demilade Kareem <demilade.o.demilade@gmail.com>
2026-08-26 07:55:22 +0100

    feat(config): add root gitignore and track SQL directory

---
+++ 03_SQL/01_database/01_create_database_schemas.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE MarketplaceIntelligence;
+GO
+
+USE MarketplaceIntelligence;
+GO
+
+CREATE SCHEMA stg;
+GO
+CREATE SCHEMA dim;      
+GO
+CREATE SCHEMA fact;       
+GO
+CREATE SCHEMA analytics;  
